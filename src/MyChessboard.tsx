576b53c7ac6e7dfe7521355536d0f5675870d029
kory <kory.zhang@outlook.com>
2023-04-01 21:14:29 +0800

    feat: validate movement use chess.js

--- src/MyChessboard.tsx
+++ src/MyChessboard.tsx
@@ -1,20 +1,46 @@
-import Chessboard from 'chessboardjsx';
-import { useState } from 'react';
-import './App.css';
+import Chessboard from "chessboardjsx";
+import { useRef, useState } from "react";
+import { Chess, Square } from "chess.js";
+import "./App.css";
 
 export const MyChessboard = (): JSX.Element => {
-  const [ position, setPosition ] = useState('start');
+  const chessGameRef = useRef(new Chess());
+  const [position, setPosition] = useState("start");
   return (
-      <Chessboard position={position} onPieceClick={(piece) => {
+    <Chessboard
+      position={position}
+      onPieceClick={(piece) => {
         console.log(`Piece clicked: ${piece}`);
-      }} onDrop={(args) => {
-        const {sourceSquare, targetSquare, piece} = args;
-        // Piece dropped: {"sourceSquare":"h2","targetSquare":"h4","piece":"wP"}
-        console.log(`Piece dropped: ${JSON.stringify(args)}`);
-      }} 
-      getPosition={((position)=>{
-        console.log(`Position: ${JSON.stringify(position)}`);
-      })}/>
-  );
-}
+      }}
+      onDrop={(args) => {
+        const { sourceSquare, targetSquare, piece } = args;
+        try {
+          chessGameRef.current.move({
+            from: sourceSquare,
+            to: targetSquare,
+          });
+          setPosition(chessGameRef.current.fen());
+        } catch (e) {
+          console.log(`Invalid move: ${e}`);
+        }
+      }}
+      allowDrag={(args) => {
+        const { piece, sourceSquare } = args;
+        // do not pick up pieces if the game is over
+        if (chessGameRef.current.isGameOver()) return false;
 
+        // only pick up pieces for the side to move
+        if (
+          (chessGameRef.current.turn() === "w" && piece.search(/^b/) !== -1) ||
+          (chessGameRef.current.turn() === "b" && piece.search(/^w/) !== -1)
+        ) {
+          return false;
+        }
+        return true;
+      }}
+      getPosition={(position) => {
+        console.log(`Position: ${JSON.stringify(position)}`);
+      }}
+    />
+  );
+};
